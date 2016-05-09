204e60fe16d47534ed0b7b598a8854d527409a22
Onat Yiğit Mercan <onatmercan@gmail.com>
2016-05-09 22:24:15 +0300

    updates nuget version

--- src/shared/SharedAssemblyInfo.cs
+++ src/shared/SharedAssemblyInfo.cs
@@ -1,14 +1,7 @@
 ﻿using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyCompany("Onat Yiğit Mercan")]
 [assembly: AssemblyCopyright("Copyright © Onat Yiğit Mercan 2014")]
-
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyConfiguration("")]
 
 [assembly: AssemblyVersion("1.1.0")]
 [assembly: AssemblyFileVersion("1.1.0")]
